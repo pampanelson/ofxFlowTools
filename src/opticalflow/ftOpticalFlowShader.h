7303436b75495bc37e4dab70c0aa73f79badefc5
pampa nelson <pampa.nelson@gmail.com>
2018-08-06 21:48:47 +0800

    change back original

--- src/opticalflow/ftOpticalFlowShader.h
+++ src/opticalflow/ftOpticalFlowShader.h
@@ -78,8 +78,6 @@ namespace flowTools {
 										   
 										   // set color
 										   gl_FragColor = vec4(flow, 0.0, 1.0);
-										   
-										   // nothing to do with fluid flow color ========== pampa
 
 
 									   }
@@ -146,9 +144,10 @@ namespace flowTools {
 									  flow *= vec2(force);
 									  
 									  // set color
-									  // nothing to do with fluid flow color ========== pampa
+								
 									  fragColor = vec4(flow, 0.0, 1.0);
-									  
+
+
 								  }
 								  
 								  );
